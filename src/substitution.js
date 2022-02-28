c19416b4b97670d6b063cf85eae105d4f9b87a42
brettsullivan1 <98278613+brettsullivan1@users.noreply.github.com>
2022-02-27 22:04:25 -0500

    Decoder Project, ...

---
+++ src/substitution.js
@@ -0,0 +1,49 @@
+const substitutionModule = (function () {
+  function substitution(input, alphabet, encode = true) {
+    if (!alphabet || alphabet.length !== 26) return false;
+    const alphabetTestArray = alphabet.split("");
+    for (let letter of alphabetTestArray) {
+      let letterCount = alphabetTestArray.filter(
+        (character) => character === letter
+      ).length;
+      if (letterCount > 1) return false;
+    }
+    const normalAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    const substitutionAlphabet = alphabet;
+    input = input.toLowerCase();
+    const splitArray = input.split("");
+    const encodedArray = [];
+
+    function findAlphabetIndexOfCharacter(array, checkAlphabet, foundAlphabet) {
+      for (let character of array) {
+        if (checkAlphabet.includes(character)) {
+          const foundIndex = checkAlphabet.indexOf(character);
+          character = foundAlphabet[foundIndex];
+        }
+        encodedArray.push(character);
+      }
+    }
+
+    if (!encode) {
+      findAlphabetIndexOfCharacter(
+        splitArray,
+        substitutionAlphabet,
+        normalAlphabet
+      );
+    } else {
+     
+      findAlphabetIndexOfCharacter(
+        splitArray,
+        normalAlphabet,
+        substitutionAlphabet
+      );
+    }
+    return encodedArray.join("");
+  }
+
+  return {
+    substitution,
+  };
+})();
+
+module.exports = { substitution: substitutionModule.substitution };
